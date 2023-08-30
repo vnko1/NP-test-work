eb40793773dfd5e6ebe78c903d71399b60c15869
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-08-30 13:06:10 +0300

    save

--- src/pages/TrackingPage.jsx
+++ src/pages/TrackingPage.jsx
@@ -1,16 +1,9 @@
 import TrackingModule from "/src/components/TrackingModule/TrackingModule";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Google } from "./Google";
+
 const TrackingPage = () => {
   return (
     <>
-      <GoogleOAuthProvider
-        clientId={
-          "557632704618-i3f3uv95cg279ru25c33sgohpcgu53k8.apps.googleusercontent.com"
-        }>
-        <TrackingModule />
-        <Google />
-      </GoogleOAuthProvider>
+      <TrackingModule />
     </>
   );
 };
